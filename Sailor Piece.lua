ae95f4f25e7dabb00ba68d6907fafeec018f4b50
rickhub940-ai <rickhub940@gmail.com>
2026-03-16 23:32:37 +0700

    Update Sailor Piece.lua

--- Sailor Piece.lua
+++ Sailor Piece.lua
@@ -502,10 +502,19 @@ task.spawn(function()
                 ReplicatedStorage.CombatSystem.Remotes.RequestHit:FireServer()
             end
         else
-    TargetMob = nil
-        end
-     end
+            TargetMob = nil
+
+            if (root.Position - quest.posQ).Magnitude > 15 then
+                if not isTweening then
+                    TweenTo(quest.posQ + Vector3.new(0,3,0))
+                end
+            end
+        end   
+
+    end   
+
 end)
+
 
 -- ----------
 -- ออโต้ถือ
